ad12e59d3dbf28c4c0c54b3b24cb8b22e179006b
Carlos <socarlosb@gmail.com>
2020-04-05 00:11:08 +0100

    add more clans

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ const app = new App({
       "9CL90JVJ",
       "9LU2Y8LU",
       "9CGQ29LR",
+      "Y0RC9CRJ",
+      "PQQURYYP",
+      "PL20RYLV",
+      "P8JYVYGJ",
+      "PJV8900L",
+      "L8JGY8YC",
+      "LL2RVR08",
     ],
   },
 });
